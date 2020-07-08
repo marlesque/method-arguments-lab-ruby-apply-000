3ac2a7cd8973f0ed09a5ebe8654a3bc40936d944
candescent-method-1839 <mqu000@citymail.cuny.edu>
2020-07-08 14:32:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,8 @@
+def introduction(name)
+  puts "Hi, my name is #{name} and I am learning to program in #{lan}."
+end
+
+
 def introduction(name, lan)
   puts "Hi, my name is #{name} and I am learning to program in #{lan}."
 end
